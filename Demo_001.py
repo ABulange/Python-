4bfb7898b358b8adff2048e47798efb48089d180
arbulange <arbulange@squaretrade.com>
2021-11-28 00:26:11 +0530

    while loop practice

---
+++ Demo_001.py
@@ -0,0 +1,58 @@
+import sys
+# x: int = (sys.argv[1])
+# y = int(sys.argv[2])
+# z = x + y
+# print (z)
+
+# i = 1
+# while i<=5:
+#     print("Telusko")
+#     i =i+1 --Adding 1 numer each time to i , i +1,i+2...i+4
+
+# i = 5
+# while i>=1:
+#     print("Try while")
+#     i =i-1 ---Reverse order -5,-4,-3,-2,-1,0
+#
+# i = 5
+# j = 1
+# while i>=1:
+#     print("telusko")
+#     while j>=5:
+#      print("rocks")
+#      j=j+1
+#     i =i-1
+
+# u=3
+# while u<=10:
+#     print ("I am new")
+#     u = u+1
+#
+# u=10
+# while u<=10:
+#     print ("I am new")
+#     u = u+1
+
+# u=3
+# while u>=1:
+#     print ("I am new")
+#     u = u-1
+
+# W=5
+# while W>=1:
+#     print("I am ", end='')
+#     R=1
+#     while R<=5:
+#         print("learning python ", end= '')
+#         R=R+1
+#     W=W-1
+#     print()
+ ===How to print both while loop in same line ? =====
+W=5
+while W>=1:
+    print("I am ", end='')
+    R=1
+    while R<=5:
+        print("learning python ")
+        R=R+1
+    W=W-1
